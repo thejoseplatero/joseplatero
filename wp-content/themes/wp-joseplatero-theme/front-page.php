d380451206ed197209f36310fbc714df353a8488
Jose Platero <contact@joseplatero.com>
2014-05-24 10:46:35 -0400

    loop on post and front page

--- wp-content/themes/wp-joseplatero-theme/front-page.php
+++ wp-content/themes/wp-joseplatero-theme/front-page.php
@@ -2,4 +2,14 @@
 
 <p>This is the front-page.php file</p>
 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  
+  <h2><?php the_title(); ?></h2>
+  <?php the_content(); ?>
+  <hr>
+
+<?php endwhile; else: ?>
+  <p>There are no posts or pages</p>
+<?php endif; ?>
+
 <?php get_footer();  ?>
---
+++ wp-content/themes/wp-joseplatero-theme/home.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<p>This is the home.php file</p>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  
+  <h2><?php the_title(); ?></h2>
+  <?php the_content(); ?>
+  <hr>
+
+<?php endwhile; else: ?>
+  <p>There are no posts or pages</p>
+<?php endif; ?>
+
+<?php get_footer();  ?>
