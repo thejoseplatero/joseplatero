4fc8bf087e6826a23c1b6f6aa44c1907efa3203b
Jose Platero <contact@joseplatero.com>
2014-05-26 20:17:24 -0400

    add links to  css

--- wp-content/themes/wp-joseplatero-theme/functions.php
+++ wp-content/themes/wp-joseplatero-theme/functions.php
@@ -4,12 +4,12 @@
  function theme_styles() {
  
  	wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css' );
-  	wp_enqueue_style( 'social', get_template_directory_uri() . '/css/webfonts.css' );
-  	wp_enqueue_style( 'goglefont-opensans', 'http://fonts.googleapis.com/css?family=Open+Sans' );
- 	wp_enqueue_style( 'grid', get_template_directory_uri() . '/css/grid.css' );
- 	wp_enqueue_style( 'main', get_template_directory_uri() . '/css/style.css' );
+  	wp_enqueue_style( 'grid', get_template_directory_uri() . '/css/grid.css' );
+ 	wp_enqueue_style( 'goglefonts', 'http://fonts.googleapis.com/css?family=Sorts+Mill+Goudy:400,400italic' );
+ 	wp_enqueue_style( 'main', get_template_directory_uri() . '/style.css' );
  	wp_enqueue_style( 'social', get_template_directory_uri() . '/css/webfonts/ss-social.css' );
- }
+
+}
 
  add_action('wp_enqueue_scripts', 'theme_styles');
 
