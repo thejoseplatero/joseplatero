8c26d7d656a414fa74dfbb9acf7abcb3fd6e142e
Jose Platero <contact@joseplatero.com>
2014-05-31 20:10:16 -0400

    styling

--- wp-content/themes/wp-joseplatero-theme/footer.php
+++ wp-content/themes/wp-joseplatero-theme/footer.php
@@ -1,5 +1,31 @@
 <footer>
-	<p>This is the footer</p>
+<div class="grid_12 omega clearfix">
+  <div id="twitter" class="grid_4">
+    <h5>Twitter</h5>
+    <p>We're accepting submissions for the next episode of Treeviews! Submit your website/app for feedback! <a href="#">trhou.se/treeviews</a></p>
+    
+    <a href="#" class="post-link">Follow me on Twitter &rarr;</a>
+  </div>
+  <div id="dribbble" class="grid_4">
+    <h5>Dribbble</h5>
+    <p>Dribbble Shots</p>
+  </div>
+  <div id="treehouse" class="grid_4 omega">
+    <h5>Treehouse</h5>
+    <p>Treehouse Badges</p>
+  </div>
+</div>
+      <div id="copyright">
+    <p>&copy; Copyright <?php echo date('Y'); ?><a href="#"> Treehouse</a>. All Rights Reserved.</p>
+  <div class="grid_12 ss-icon omega">
+      <a href="#">&#xF610;</a>
+      <a href="#">&#xF611;</a>
+      <a href="#">&#xF612;</a>
+      <a href="#">&#xF613;</a>
+      <a href="#">&#xF660;</a>
+      <a href="#">&#x2709;</a>
+  </div>
+  </div>
 	<?php wp_footer(); ?>
 </footer>	
 	</body>
--- wp-content/themes/wp-joseplatero-theme/header.php
+++ wp-content/themes/wp-joseplatero-theme/header.php
@@ -11,16 +11,30 @@
 			 ?>
 
 		</title>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0">
 		<?php wp_head(); ?>
 	</head>
 	<body>
 
-		<p>This is the header</p>
+		<div class="container clearfix">
+      <header>
+        <div class="grid_12 omega none">
+          <hgroup>
+            <h1><a href="<?php bloginfo('siteurl'); ?>"> <?php bloginfo('name'); ?></a></h1>
+            <h2><?php bloginfo('description'); ?></h2>
+          </hgroup>
+        </div>
+        <div class="grid_12 omega">
+          <nav>
 
-		<?php 
-		    $args = array(
-		        'menu' => 'main-menue'
-		      );
+        		<?php 
+        		    $args = array(
+        		        'menu' => 'main-menu',
+                     'echo'=> false
+        		      );
 
-		    wp_nav_menu( $args);
-		?>
+        		      echo strip_tags(wp_nav_menu( $args ), '<a>');
+        		?>
+          </nav>
+        </div>
+      </header>
